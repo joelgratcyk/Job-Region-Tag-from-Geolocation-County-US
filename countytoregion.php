02c1b648c1fddd951ff5b22c73a584ce11d7bb01
Joel Gratcyk <joel@gratcyk.com>
2023-07-25 06:34:41 -0500

    Update countytoregion.php
    Updated name to include US

--- countytoregion.php
+++ countytoregion.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Plugin Name: Job Region Tag from Geolocation County
+ * Plugin Name: Job Region Tag from Geolocation County US
  * Plugin URI: https://github.com/joelgratcyk
  * Description: This plugin automatically adds a _job_region tag to job_listing posts based on their geolocation_county meta value, but only for job listings in the US.
  * Version: 1.0
